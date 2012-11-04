9cfa1a17e6546723d61405dcf752b6677ce761cd
Alberto Díaz <dmalber@gmail.com>
2012-11-04 21:12:22 +0100

    Listener de servlets para el framework
    Signed-off-by: Alberto Díaz <dmalber@gmail.com>

---
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/servlet/FormworkListener.java
@@ -0,0 +1,18 @@
+package edu.uoc.pfc.formwork.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class FormworkListener implements ServletContextListener {
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
