6fc6ab21fdc2cd3bf7ff43b3ccb1efac264646c5
Alberto Díaz <dmalber@gmail.com>
2012-11-04 01:26:53 +0100

    Prueba del loader
    Signed-off-by: Alberto Díaz <dmalber@gmail.com>

--- formworkcore/src/main/java/edu/uoc/pfc/formwork/servlet/FormworkLoaderServlet.java
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/servlet/FormworkLoaderServlet.java
@@ -1,11 +1,16 @@
 package edu.uoc.pfc.formwork.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBException;
+
+import edu.uoc.pfc.formwork.xml.TipoFormulario;
+import edu.uoc.pfc.formwork.xml.XMLLoader;
 
 @SuppressWarnings("serial")
 public class FormworkLoaderServlet extends HttpServlet {
@@ -17,6 +22,15 @@ public class FormworkLoaderServlet extends HttpServlet {
 	}
 	
 	private void load(HttpServletRequest req, HttpServletResponse resp) {
+		InputStream resource = getServletContext().getResourceAsStream("/index.fwp");
 		
+		try {
+			TipoFormulario formulario = XMLLoader.parseFwp(TipoFormulario.class, resource);
+			System.out.println("formulario: " + formulario);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
