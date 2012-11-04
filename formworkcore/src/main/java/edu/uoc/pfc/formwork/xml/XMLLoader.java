075422887b4326c5775557d35c012ad62e6bf6a6
Alberto Díaz <dmalber@gmail.com>
2012-11-04 01:12:34 +0100

    Se añade XMLLoader
    Signed-off-by: Alberto Díaz <dmalber@gmail.com>

---
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/xml/XMLLoader.java
@@ -0,0 +1,29 @@
+package edu.uoc.pfc.formwork.xml;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class XMLLoader {
+	private XMLLoader() {
+	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T parseFwp(Class<T> docClass, InputStream thePage) 
+				throws JAXBException {
+		String packageName = docClass.getPackage().getName();
+		JAXBContext context = JAXBContext.newInstance(packageName);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		JAXBElement<T> doc = (JAXBElement<T>) unmarshaller.unmarshal(thePage);
+		return doc.getValue();
+	}
+	
+	public static <T> T  parseFwp(Class<T> docClass, URL thePage) {
+		return null;
+	}
+}
