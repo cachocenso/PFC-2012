9b153e5eb56b4c7698dff88dd84175b2fa80fa88
Alberto Díaz <dmalber@gmail.com>
2012-11-08 19:43:41 +0100

    Más componentes gráficos.
    Signed-off-by: Alberto Díaz <dmalber@gmail.com>

---
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/PartidaCadena.java
@@ -0,0 +1,11 @@
+package edu.uoc.pfc.formwork.ui;
+
+public class PartidaCadena extends Partida<String> {
+
+	@Override
+	public void render(IRenderer renderer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/PartidaLista.java
@@ -0,0 +1,43 @@
+package edu.uoc.pfc.formwork.ui;
+
+import java.util.List;
+
+
+public class PartidaLista extends Partida<String> {
+
+	
+	private int indexSeleccionado;
+	private List<String> values;
+	
+	@Override
+	public void render(IRenderer renderer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getIndexSeleccionado() {
+		return indexSeleccionado;
+	}
+
+	public void setIndexSeleccionado(int indexSeleccionado) {
+		this.indexSeleccionado = indexSeleccionado;
+	}
+
+	public List<String> getValues() {
+		return values;
+	}
+
+	public void setValues(List<String> values) {
+		this.values = values;
+	}
+	
+	@Override
+	public String getValue() {
+		if (values == null || indexSeleccionado < 0 || indexSeleccionado >= values.size()) {
+			return null;
+		}
+		
+		return values.get(indexSeleccionado);
+	}
+
+}
