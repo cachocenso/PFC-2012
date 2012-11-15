f3622a8dd9017d946b548efc00f34343c90375d9
Alberto Díaz <dmalber@gmail.com>
2012-11-15 13:30:17 +0100

    Sistema de renderizado de componentes.

--- formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/HTMLRenderer.java
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/HTMLRenderer.java
@@ -2,4 +2,14 @@ package edu.uoc.pfc.formwork.ui;
 
 public class HTMLRenderer implements IRenderer {
 
+	/* (non-Javadoc)
+	 * @see edu.uoc.pfc.formwork.ui.IRenderer#render(edu.uoc.pfc.formwork.ui.Componente)
+	 */
+	public void render(Componente root) {
+		
+		if (!(root instanceof Formulario)) {
+			throw new IllegalArgumentException("Se esperaba un objeto Formulario");
+		}
+	}
+
 }
--- formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/IRenderer.java
+++ formworkcore/src/main/java/edu/uoc/pfc/formwork/ui/IRenderer.java
@@ -1,5 +1,10 @@
 package edu.uoc.pfc.formwork.ui;
 
 public interface IRenderer {
-
+	/**
+	 * Dibuja el contenido del árbol de componentes al lenguaje
+	 * especificado en la clase que implementa la interfaz
+	 * @param root
+	 */
+	void render(Componente root);
 }
